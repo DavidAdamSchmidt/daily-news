205a73a5a370cb8648ddf2eb3b0b388a9852a105
David Schmidt <david.adam.schmidt@gmail.com>
2020-03-21 00:31:16 +0100

    Add search box

---
+++ DailyNews/DailyNews/Pages/NewsBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace DailyNews.Pages
+{
+    public class NewsBase : ComponentBase
+    {
+        public List<string> NewsList { get; } = new List<string>();
+
+        public string SearchTerm { get; set; } = string.Empty;
+
+        public bool IsUpdateDisabled => SearchTerm.Length == 0;
+
+        public void UpdateNews()
+        {
+            NewsList.Add(SearchTerm);
+        }
+    }
+}
